d6ed9cfc58412e0ffa2bd21a2b0f6e3b1be6644c
Geert Pasteels <geert.pasteels@gmail.com>
2013-02-11 16:13:39 +0100

    jungles

---
+++ jungles.pp
@@ -0,0 +1,25 @@
+package { 'software-properties-common': }
+
+exec { 'node repo': 
+  command => '/usr/bin/apt-add-repository ppa:chris-lea/node.js',
+  require => Package['software-properties-common']
+}
+
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+  require => Exec['node repo']
+}
+
+package { 'nodejs':
+  require => Exec['apt-get update']
+}
+
+package { 'npm':
+  require => Exec['apt-get update']
+}
+
+exec { 'startup':
+  command => '/usr/bin/curl https://raw.github.com/Enome/startup/master/bin/startup > /usr/local/bin/startup',
+}
+
+package { 'postgresql': }
