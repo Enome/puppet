3c13ae4ee3a328d78174226d1ed3163d156f4f7c
Geert Pasteels <geert.pasteels@gmail.com>
2013-02-11 17:22:04 +0100

    added +x for startup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jungles
+
+```
+apt-get install puppet && curl https://raw.github.com/Enome/puppet/master/jungles.pp | puppet apply
+```
--- jungles.pp
+++ jungles.pp
@@ -18,9 +18,16 @@ package { 'npm':
   require => Exec['apt-get update']
 }
 
+
 exec { 'startup':
   command => '/usr/bin/curl https://raw.github.com/Enome/startup/master/bin/startup > /usr/local/bin/startup',
 }
 
+exec { 'startup executable':
+  command => '/bin/chmod +x /usr/local/bin/startup',
+  require => Exec['startup']
+}
+
 package { 'postgresql': }
 package { 'git': }
+package { 'build-essential': }
