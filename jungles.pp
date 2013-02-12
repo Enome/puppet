f255655571ce411a56dca635dcd15dc26509bc7f
Geert Pasteels <geert.pasteels@gmail.com>
2013-02-12 16:17:23 +0100

    added iptables to redirect port 80 => 3000

--- jungles.pp
+++ jungles.pp
@@ -31,3 +31,7 @@ exec { 'startup executable':
 package { 'postgresql': }
 package { 'git': }
 package { 'build-essential': }
+
+exec { 'iptables':
+  command => '/sbin/iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000 -v'
+}
