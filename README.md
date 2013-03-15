9b8628a13cd3fe9173a8c666586648dacabe3be3
Geert Pasteels <geert.pasteels@gmail.com>
2013-03-15 15:26:17 +0100

    create postgres user

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Jungles
 
 ```
-apt-get install puppet && curl https://raw.github.com/Enome/puppet/master/jungles.pp | puppet apply
+apt-get install puppet && puppet module install akumria/postgresql && curl https://raw.github.com/Enome/puppet/master/jungles.pp | puppet apply
 ```
--- jungles.pp
+++ jungles.pp
@@ -1,3 +1,5 @@
+# Node.js
+
 package { 'software-properties-common': }
 
 exec { 'node repo': 
@@ -14,6 +16,9 @@ package { 'nodejs':
   require => Exec['apt-get update']
 }
 
+
+# Startup
+
 exec { 'startup':
   command => '/usr/bin/curl https://raw.github.com/Enome/startup/master/bin/startup > /usr/local/bin/startup',
 }
@@ -23,16 +28,41 @@ exec { 'startup executable':
   require => Exec['startup']
 }
 
-package { 'postgresql': }
+# Git
 package { 'git': }
+
+
+# G++ for unicode module (slug)
+
 package { 'build-essential': }
-package { 'ufw': }
+
+
+# Iptables
 
 exec { 'iptables':
   command => '/sbin/iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000 -v'
 }
 
+
+# Firewall
+
+package { 'ufw': }
+
 exec { 'ufw-setup':
   command => '/usr/sbin/ufw allow ssh && /usr/sbin/ufw allow ssh && /usr/sbin/ufw allow 80 && /usr/sbin/ufw --force enable',
   require => Package['ufw']
+}
+
+
+# Postgres
+
+class {'postgresql::server': 
+  listen => ['localhost',],
+  acl => ['local all all trust', ],
+}
+
+postgresql::db { 'jungles':
+  password => '1234',
+  owner => 'jungles',
+  require => Service['postgresql-system-9.1'],
 }
